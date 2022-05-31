27f1c38d036caed23791a8ff179bea8ff56a69cb
Chi-Han Huang <huangchihan@gmail.com>
2022-05-31 15:28:07 +0800

    Minor tweak.

--- internal/app/games/routes.go
+++ internal/app/games/routes.go
@@ -1,12 +1,11 @@
 package games
 
 import (
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Routes(r *gin.RouterGroup) {
-	g := r.Group("/games", cors.Default())
+	g := r.Group("/games")
 
 	g.GET("", GetGames)
 	g.GET("/:game_bundle_id/products", GetProducts)
